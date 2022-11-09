c8277b0bb221ac8cba798d8c3174705a81f24fa0
OzFounry <68696228+OzFounry@users.noreply.github.com>
2022-11-09 11:41:58 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -4,12 +4,6 @@ const markdownIt = require("markdown-it");
     const md = new markdownIt({
     html: true,
     });
-addPlugin
-            const pluginRss = require("@11ty/eleventy-plugin-rss");
-
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(pluginRss);
-};
 
       const createCollectionsAndFilters = require('./_utils/index.js');
       
